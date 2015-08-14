40c40367b6fd6fb8c705ee272c99270dbc14aad7
maxprzemo <przemekbanasiak1@wp.pl>
2015-08-14 08:24:07 +0200

    Fix manual network search
     Changes to be committed:
    	modified:   BoardConfig.mk
    	modified:   device_g2m.mk
    	new file:   ril/telephony/java/com/android/internal/LgeRIL.java

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,6 +87,9 @@ USE_OPENGL_RENDERER := true
 # Hardware tunables framework
 BOARD_HARDWARE_CLASS := device/lge/g2m/cmhw/
 
+# RIL
+BOARD_RIL_CLASS := ../../../device/lge/g2m/ril/
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/g2m/bluetooth
 BOARD_HAVE_BLUETOOTH := true
